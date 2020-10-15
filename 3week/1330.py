4332460c7b4f4a8dca715364882bf52b78993476
이미라 <emira1239@naver.com>
2020-10-15 19:55:37 +0900

    3week

---
+++ 3week/1330.py
@@ -0,0 +1,32 @@
+#첫 번째
+A = 1
+B = 2
+
+if A > B:
+    print("A > B")
+elif A < B:
+    print("A < b")
+else:
+    print(A == B)
+
+#두 번째
+A = 10
+B = 2
+
+if A > B:
+    print("A > B")
+elif A < B:
+    print("A < b")
+else:
+    print(A == B)
+
+#세 번째
+A = 5
+B = 5
+
+if A > B:
+    print("A > B")
+elif A < B:
+    print("A < b")
+else:
+    print(A == B)
