e42865eee3f5b22ca67479cf77a34c9fdd6b1242
이미라 <emira1239@naver.com>
2020-10-29 20:29:47 +0900

    4week

---
+++ 4week/2588.py
@@ -0,0 +1,32 @@
+print("곱셈을 진행할 세 자리 자연수를 두 개 입력해 주세요")
+'''
+A = 472
+X = 385
+
+a = A // 100
+b = A % 100 // 10
+c = A % 10 // 1
+
+x = X // 100
+y = X % 100 // 10
+z = X % 10 // 1
+
+print(A * x)
+print(A * y)
+print(A * z)
+print(A * z * 100 + A * y * 10 + A * z)
+'''
+A = int(input())
+a = A // 100
+b = A % 100 // 10
+c = A % 10 // 1
+X = int(input())
+x = X // 100
+y = X % 100 // 10
+z = X % 10 // 1
+
+print(A, "*", x, "=", A*x)
+print(A, "*", y, "=", A*y)
+print(A, "*", z, "=", A*y)
+
+print(A, "*", X, "=", A*x*100+A*y*10+A*z)
---
+++ 4week/2884.py
@@ -0,0 +1,15 @@
+print('''상근이가 일어나고자 하는 시간과 분을 입력하시오
+(0<=H<=23, 0<=M<=59)''')
+
+H = int(input())
+M = int(input())
+
+minute = H * 60 + M
+todo = minute - 45
+
+if todo < 0:
+    todo = todo + 1400
+
+resulthour = todo // 60
+resultminute = todo % 60
+print("창영이의 방법 : ", resulthour, "시", resultminute, "분")
